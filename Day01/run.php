38f6fb482ef901c15e8f4f65e42241cb2903eace
pwnstar <mikeroq@gmail.com>
2020-12-01 14:09:00 -0600

    Added execution time.

--- Day01/run.php
+++ Day01/run.php
@@ -1,11 +1,13 @@
 <?php 
 require("class/Day01.php");
 use pwnstar\AdventOfCode\Day01;
-
+$start_time = microtime(true);
 $day01 = new Day01("input.txt", 2020);
 
-echo "First Answer: ";
+echo 'First Answer: ';
 echo $day01->findFirstAnswer() . PHP_EOL;
 
-echo "Second Answer: ";
+echo 'Second Answer: ';
 echo $day01->findSecondAnswer() . PHP_EOL;
+
+echo 'Execution finished in ' . (microtime(true) - $start_time) . ' seconds.';

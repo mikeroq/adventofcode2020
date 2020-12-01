aa1c0ff398e3c12fbba4b1773dd4f603c575f49e
pwnstar <mikeroq@gmail.com>
2020-12-01 08:24:43 -0600

    Solved

--- Day01/run.php
+++ Day01/run.php
@@ -2,10 +2,10 @@
 require("class/Day01.php");
 use pwnstar\AdventOfCode\Day01;
 
-$findPairs = new Day01("input.txt", 2020);
+$day01 = new Day01("input.txt", 2020);
 
 echo "First Answer: ";
-echo $findPairs->findFirstAnswer() . PHP_EOL;
+echo $day01->findFirstAnswer() . PHP_EOL;
 
 echo "Second Answer: ";
-echo $findPairs->findSecondAnswer() . PHP_EOL;
+echo $day01->findSecondAnswer() . PHP_EOL;

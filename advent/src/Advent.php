04480409ec08ddfc874f11b687f39eb9a097d7d7
pwnstar <mikeroq@gmail.com>
2020-12-01 21:45:46 -0600

    Ready

--- advent/src/Advent.php
+++ advent/src/Advent.php
@@ -2,8 +2,6 @@
 namespace pwnstar\AdventOfCode2020;
 require_once "vendor/autoload.php";
 
-use pwnstar\AdventOfCode2020\Day01;
-
 class Advent 
 {
     public function runDay(string $dayClass, string $input, $assert)
@@ -11,7 +9,6 @@ class Advent
         $start_time = microtime(true);
         $class = 'pwnstar\AdventOfCode2020\\' . $dayClass . '\\' . $dayClass;
         $day = new $class($input, $assert);
-
         echo 'First Answer: ' . $day->findFirstAnswer() . PHP_EOL
             . 'Second Answer: ' . $day->findSecondAnswer() . PHP_EOL
             . 'Execution finished in ' . (microtime(true) - $start_time) . ' seconds.';
